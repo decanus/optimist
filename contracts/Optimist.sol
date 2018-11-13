6236ab2371363803194b0ced4b06c407bfd55aff
decanus <dean.eigenmann@icloud.com>
2018-11-13 17:53:53 +0100

    started minor implementation work

--- contracts/Optimist.sol
+++ contracts/Optimist.sol
@@ -4,21 +4,30 @@ contract Optimist {
 
     uint256 public stake;
 
+    bytes4 public proofFunction;
+    bytes4 public submissionFunction;
+
     struct Data {
         bytes input;
         address sender;
     }
 
-    constructor(uint _stake, bytes4 _proofFunction, bytes4 _submissionFunction) {
+    constructor(uint _stake, bytes4 _proofFunction, bytes4 _submissionFunction) public {
         stake = _stake;
+        proofFunction = _proofFunction;
+        submissionFunction = _submissionFunction;
     }
 
     function commit(bytes input) external payable {
         require(msg.value == stake, "incorrect stake amount sent.");
+
+        // @todo put data into mapping of contract
     }
 
     function challenge() external {
+        /// @todo call verifier function
 
+        msg.sender.transfer(stake);
     }
 
 }
