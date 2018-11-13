5b8f9a1707363db5c2ab0296305fffb600f51456
decanus <dean.eigenmann@icloud.com>
2018-11-13 03:58:21 +0100

    added optimist base

---
+++ contracts/Optimist.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.4.18;
+
+contract Optimist {
+
+    uint256 public stake;
+
+    struct Data {
+        bytes input;
+        address sender;
+    }
+
+    constructor(uint _stake) {
+        stake = _stake;
+    }
+
+    function commit(bytes input) external {
+
+    }
+
+    function challenge() external {
+        
+    }
+
+}
