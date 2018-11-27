b860dff7162d90333472f2f087f0f85d9b160839
decanus <dean.eigenmann@icloud.com>
2018-11-27 19:34:36 +0900

    minor code style

--- test/mocks/DataStorageMock.sol
+++ test/mocks/DataStorageMock.sol
@@ -5,10 +5,10 @@ import "../../contracts/DataStorage.sol";
 contract DataStorageMock is DataStorage {
 
     function isValid(bytes input) external returns (bool) {
-        return input[0] == 1;
+        return input[0] == 0x01;
     }
 
-    function submit(bytes input) external {
+    function submit(bytes) external {
         // @todo
     }
 }
