9d55506143eec271bacff5a2ad70ba47fe2d4ad7
decanus <dean.eigenmann@icloud.com>
2018-11-27 21:41:06 +0900

    semi colons

--- test/helpers/Utils.js
+++ test/helpers/Utils.js
@@ -8,7 +8,7 @@ function ensureException(error) {
 }
 
 function increaseTime(duration) {
-    const id = Date.now()
+    const id = Date.now();
 
     return new Promise((resolve, reject) => {
         web3.currentProvider.sendAsync({
@@ -17,17 +17,17 @@ function increaseTime(duration) {
             params: [duration],
             id: id,
         }, err1 => {
-            if (err1) return reject(err1)
+            if (err1) return reject(err1);
 
             web3.currentProvider.sendAsync({
                 jsonrpc: '2.0',
                 method: 'evm_mine',
                 id: id+1,
             }, (err2, res) => {
-                return err2 ? reject(err2) : resolve(res)
-            })
-        })
-    })
+                return err2 ? reject(err2) : resolve(res);
+            });
+        });
+    });
 }
 
 module.exports = {
